0bf1aae0a29c7c4fc7aa6233df933b17ef5f1ac7
Mauricio Juárez <iconoclastsym@gmail.com>
2023-11-11 23:47:33 -0600

    fixing files

--- components/helloWorld.js
+++ components/helloWorld.js
@@ -11,22 +11,27 @@ module.exports = {
   invoke: (conversation, done) => {
  
    const {variable} = conversation.properties();
+   conversation.logger().info('Variable: ' + variable);
 
-	let attachment = conversation.attachment();
-    let fileurl = attachment != null? attachment.url : null;
-    conversation.logger().info('Attachment URL: ' + fileurl);
+   let attachment = conversation.attachment();
+   conversation.logger().info('Attachment: ' + JSON.stringify(attachment));
+   
+   // If the attachment is from Facebook Messenger, get the URL from the payload
+   let fileurl = attachment && attachment.payload ? attachment.payload.url : null;
+   conversation.logger().info('Attachment URL: ' + fileurl);
     
-    if(fileurl != null){
-      conversation.logger().info('Attachment URL type: ' + typeof fileurl); 
-      conversation.variable(variable, fileurl);
-      conversation.transition('success');  
-      conversation.keepTurn(true);
-      done();
-    }
-    else{
-      conversation.transition('error');  
-      conversation.keepTurn(true);
-      done();    
-    }
+   if(fileurl != null){
+     conversation.logger().info('Attachment URL type: ' + typeof fileurl); 
+     conversation.variable(variable, fileurl);
+     conversation.transition('success');  
+     conversation.keepTurn(true);
+     done();
+   }
+   else{
+     conversation.logger().error('No attachment found or URL is null');  
+     conversation.transition('error');  
+     conversation.keepTurn(true);
+     done();    
+   }
   }
 };
